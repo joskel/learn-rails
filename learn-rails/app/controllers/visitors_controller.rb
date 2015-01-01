568fabc71795f47514b652d7ba65d2110b097a09
joskel <j.kelleners@maasbracht.org>
2015-01-01 21:45:11 +0000

    front end framework

--- learn-rails/app/controllers/visitors_controller.rb
+++ learn-rails/app/controllers/visitors_controller.rb
@@ -3,7 +3,5 @@ class VisitorsController < ApplicationController
   def new
     Rails.logger.debug 'DEBUG : entering new method'
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon'
   end
 end
