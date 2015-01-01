b80a4e6d1be3c594d19e5e05e195560f5883e8a5
joskel <j.kelleners@maasbracht.org>
2015-01-01 23:42:42 +0000

    contact form

--- learn-rails/config/routes.rb
+++ learn-rails/config/routes.rb
@@ -55,8 +55,8 @@ Rails.application.routes.draw do
   #   end
   
   LearnRails::Application.routes.draw do
-    root to: 'visitors#new'
-    
+    resources :contacts, only: [:new, :create]  
+    root to: 'visitors#new'    
   end
   
   
